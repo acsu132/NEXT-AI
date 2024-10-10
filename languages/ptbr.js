74e01467998fd7137cf87cdc6ddb80fd7bfb0e81
Adriel882034 <152195399+Adriel882034@users.noreply.github.com>
2024-10-10 06:11:07 -0300

    Create ptbr.js

---
+++ languages/ptbr.js
@@ -0,0 +1,70 @@
+const ptLang = {
+    error: 'Ocorreu um erro ao processar seu comando.',
+    commandDisabled: 'Este comando está desativado no momento.',
+    //avatar.js
+    avatarDescription: "Mostra seu avatar com links para download",
+    avatarUserOptionDescription: "O usuário para mostrar o avatar",
+    avatarTitle: "Avatar de {username}",
+    downloadPng: "▶️ Baixar PNG",
+    downloadJpeg: "▶️ Baixar JPEG",
+
+    //channelinfo.js
+    channelInfo: "Informações do Canal",
+    channelName: "Nome do Canal",
+    channelID: "ID do Canal",
+    channelType: "Tipo",
+    channelCreatedAt: "Criado em",
+    channelTopic: "Tópico",
+    channelNSFW: "NSFW",
+    channelPosition: "Posição",
+    channelCategory: "Categoria",
+    channelNone: "Nenhum",
+    channelYes: "Sim",
+    channelNo: "Não",
+    channelMentionValid: "Mencione um canal válido, por favor.",
+    channelInfoDescription: 'Mostra informações detalhadas sobre um canal',
+    channelOptionDescription: 'O canal para obter informações',
+    //invite.js
+    inviteTitle: "Link de Convite",
+    inviteDescription: "Clique [aqui]({inviteURL}) para convidar o bot para seu servidor!",
+    inviteImageURL: "https://cdn.discordapp.com/attachments/1246408947708072027/1256597293323256000/invite.png?ex=668158ed&is=6680076d&hm=030c83f567ffdaf0bebb95e50baaec8bb8a8394fa1b7717cc43c3622447f58e3&",
+    //ping.js
+    pingDescription: "Ping!",
+    pingTitle: "Pong! 🏓",
+    botLatency: "Latência do Bot",
+    apiLatency: "Latência da API",
+    // servericon.js
+    serverIconDescription: "Mostra o ícone do servidor",
+    serverIconTitle: "Ícone do Servidor {guildName}",
+    serverIconNoIcon: "Este servidor não possui um ícone",
+    // serverinfo.js
+    serverInfoDescription: "Exibe informações sobre o servidor.",
+    serverInfoTitle: "Informações do Servidor",
+    serverInfoError: "Ocorreu um erro ao buscar as informações do servidor. Tente novamente mais tarde.",
+    serverInfoFields: {
+        serverName: "Nome do Servidor",
+        serverOwner: "Dono do Servidor",
+        serverOwnerId: "ID do Dono do Servidor",
+        serverId: "ID do Servidor",
+        members: "Membros",
+        membersNonBots: "Membros (Sem Bots)",
+        bots: "Bots",
+        serverBoosts: "Impulsos do Servidor",
+        categories: "Categorias",
+        totalChannels: "Total de Canais",
+        textChannels: "Canais de Texto",
+        voiceChannels: "Canais de Voz",
+        stageChannels: "Canais de Palco",
+        emojis: "Emojis",
+        normalEmojis: "Emojis Normais",
+        animatedEmojis: "Emojis Animados",
+        stickers: "Figurinhas",
+        roles: "Cargos",
+        serverCreatedOn: "Servidor Criado em",
+        emojisList: "Lista de Emojis",
+        rolesList: "Lista de Cargos"
+    },
+    // outros comandos seguem a mesma lógica...
+};
+
+module.exports = ptLang;
