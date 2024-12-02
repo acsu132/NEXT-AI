cb444716f4a30d2e53b84811bcafb1c7cb2b271a
Adriel882034 <152195399+Adriel882034@users.noreply.github.com>
2024-12-02 19:11:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -305,28 +305,11 @@ app.listen(port, () => {
 
 const news = require('./events/news');
 
-// Inicializar o módulo
 news.init(client);
 
-    }
-}
-
-const fs = require('fs');
-const path = require('path');
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const event = require(path.join(eventsPath, file));
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-}
+});
+const forumrepost = require('./events/forumrepost')
+});
 
 client.login(process.env.TOKEN);
 
