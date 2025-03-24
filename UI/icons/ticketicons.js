f34c20b12270fa7c6f22f7a1be2bf8f6d012c57c
Adr <152195399+acsu132@users.noreply.github.com>
2025-03-24 19:06:21 -0300

    Edit ticket icons

--- UI/icons/ticketicons.js
+++ UI/icons/ticketicons.js
@@ -1,9 +1,9 @@
 const ticketIcons = {
-    mainIcon: 'https://cdn.discordapp.com/emojis/1081651089339858954.gif', 
-    correctIcon: 'https://cdn.discordapp.com/emojis/819446784647757834.gif', 
+    mainIcon: 'https://cdn.discordapp.com/attachments/1335584527413809172/1353850905295847524/Animated_Arrow_Purple.gif?ex=67e32769&is=67e1d5e9&hm=444c3926b39fc6df184b14f3c13bfd20a0d14b6f88eb2a2212e274a1d9b1a70b&', 
+    correctIcon: 'https://cdn.discordapp.com/attachments/1335584527413809172/1353850904788471838/Purple_verify.gif?ex=67e32769&is=67e1d5e9&hm=d00f6e47fe27eb0f04b1af4ff51c602fc3a1998879f1b272ef3fd98fc49a397f&', 
     correctrIcon : 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=66a5702b&is=66a41eab&hm=5f38fab7b9dab73a6250db1a5e149b94bcdd49b19d6b70e38253fa2b2470615f&',
     heartIcon: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=66a571e8&is=66a42068&hm=0761d49758b73e8bbe8785e7998acfd2f1f79b4f623f024072468d4420cc102e&',
-    modIcon :'https://cdn.discordapp.com/emojis/805980519644004372.gif',
+    modIcon :'https://cdn.discordapp.com/attachments/1335584527413809172/1353851916614107146/1080-admin-purple.png?ex=67e3285a&is=67e1d6da&hm=fb02ab1977e988bbff830dc7108393f998a65a188a14afc8843c4c4e5e65a74f&',
 };
 
 module.exports = ticketIcons;
